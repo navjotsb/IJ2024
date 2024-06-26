1288704d09a3129f7cdb3153826497cf483ebb93
navjotsb <navjot2bhatia@gmail.com>
2024-06-25 20:58:23 -0400

    new commit

---
+++ May28/June25/src/IphoneXMain.java
@@ -0,0 +1,14 @@
+
+package june25.multipledemo;
+
+public class IphoneXMain {
+    public static void main(String[] args) {
+
+        Iphone iphone = new Iphone();
+        iphone.takePictures();
+        iphone.calculate();
+        iphone.makeCalls();
+
+
+    }
+}

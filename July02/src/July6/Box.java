4f93a4ac79038f9b0050521955e6176b6f59ec13
navjotsb <navjot2bhatia@gmail.com>
2024-07-06 14:09:57 -0400

    new commit

---
+++ July02/src/July6/Box.java
@@ -0,0 +1,14 @@
+package July6;
+
+public class Box<T,S> {
+
+    T item1;
+    S item2;
+
+    public Box(T item1, S item2) {
+        this.item1 = item1;
+        this.item2 = item2;
+
+    }
+
+}
---
+++ July02/src/July6/BoxMain.java
@@ -0,0 +1,11 @@
+package July6;
+
+public class BoxMain {
+    public static void main(String[] args) {
+
+
+         Box<String,String> box = new Box<String, String>("Iphone","Ipad");
+        System.out.println(box.item1 + " " + box.item2);
+
+    }
+}

42a43189f48abb54356c11446094b7f77acce33f
navjotsb <navjot2bhatia@gmail.com>
2024-08-15 01:45:32 -0400

    new commit

---
+++ Assignment/src/Assignment4thJuly/FunctionalInterfacePractice/GeTest.java
@@ -0,0 +1,9 @@
+package Assignment4thJuly.FunctionalInterfacePractice;
+
+public class GeTest {
+
+    public Boolean grtTest( IGreaterNo iGreaterNo , Integer n1 )
+    {
+     return  iGreaterNo.checkNo(n1);
+    }
+}
--- Assignment/src/Assignment4thJuly/FunctionalInterfacePractice/GreaterNoMain.java
+++ Assignment/src/Assignment4thJuly/FunctionalInterfacePractice/GreaterNoMain.java
@@ -6,17 +6,25 @@ public class GreaterNoMain {
 
     public static void main(String[] args) {
 
+
+      /*  IGreaterNo greaterNoPredicate = (n) -> n>10;
+        GeTest geTest = new GeTest();
+        System.out.println(geTest.grtTest(greaterNoPredicate, 8)); */
         Predicate<Integer> greaterNoPredicate = (n) -> n>10;
         System.out.println("Test Predicate: " + greaterNoPredicate.test(8));
+
         Function<String,Integer> stringIntegerFunction = (T) -> T.length();
         System.out.println("Apply Function: " + stringIntegerFunction.apply("Navjot"));
         Function<Integer,Integer> adthnCheck = (S) -> S*2;
         Function<String,Integer> combineCheck =  stringIntegerFunction.andThen(adthnCheck);
         System.out.println("addthen Function: " +combineCheck.apply("Navjot"));
+
         Consumer<String> upper = (T)-> System.out.println("accept Consumer: " + T.toUpperCase());
         upper.accept("navjot");
+
         BiFunction<Double,Double,Double> addCheck = (a,b)-> a+b;
         System.out.println("apply BiFunction: " + addCheck.apply(10.0, 5.0));
+
         Supplier<Double> randomNumberSupplier = () -> Math.random();
         System.out.println("get Supplier: " + randomNumberSupplier.get());
 
